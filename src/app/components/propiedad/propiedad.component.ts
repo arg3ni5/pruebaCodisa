a4f9e9039a6d082c24385e03cd20edaa7319fec6
Fabian Chinchilla <fb.argenis@gmail.com>
2021-02-18 17:35:31 -0600

    -- imprementacion de siguiente propiedad

--- src/app/components/propiedad/propiedad.component.ts
+++ src/app/components/propiedad/propiedad.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Propiedad } from 'src/app/interfaces/interfaces';
+import { PropiedadesService } from '../../services/propiedades.service';
 
 @Component({
   selector: 'app-propiedad',
@@ -11,12 +12,20 @@ export class PropiedadComponent implements OnInit {
   // Objeto seleccionado
   @Input() propiedad: Propiedad;
 
-  constructor() {
+  constructor(private propiedadesService: PropiedadesService) {
     if (this.propiedad == undefined) {
       this.propiedad = { title: "" };
     }
   }
   ngOnInit(): void {
+    this.propiedadesService.selectPropiedad.subscribe((propiedad: Propiedad) => {
+      console.log(propiedad);
+      this.propiedad = propiedad;
+    })
+  }
+
+  nextPropiedad() {
+    this.propiedadesService.nextPropiedad();
   }
 
 }
--- src/app/services/propiedades.service.ts
+++ src/app/services/propiedades.service.ts
@@ -1,17 +1,52 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Propiedad } from 'src/app/interfaces/interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PropiedadesService {
-  
+  selectPropiedad = new EventEmitter<Propiedad>();
+  propiedades: Propiedad[]; // lista de propiedades
+  posPropiedad: number = 0; // Posicion de propiedad mostrada
+
   constructor(private http: HttpClient) { }
 
   getPropiedades() {
-    return this.http.get('https://rtfe-test-default-rtdb.firebaseio.com/properties.json');
+    return this.http.get<Propiedad[]>('https://rtfe-test-default-rtdb.firebaseio.com/properties.json');
   }
   getImgsPropiedades(id: number) {
     return this.http.get<any>(`https://rtfe-test-default-rtdb.firebaseio.com/property_images/${id}.json`);
+  }
+  setPropiedad(propiedad: Propiedad, posPropiedad: number) {
+    this.posPropiedad = posPropiedad;
+    this.selectPropiedad.emit(propiedad);
+  }
+  nextPropiedad() {
+    this.getPropiedades().subscribe((data: Propiedad[]) => {
+
+      console.log(data);
+      let arr = [];  
+      Object.keys(data).map(function(key){  
+          arr.push({[key]:data[key]})  
+          return arr;  
+      });        
+
+      if (this.posPropiedad == data.length - 1) {
+        this.posPropiedad = 0;
+      }
+      else {
+        this.posPropiedad++;
+      }
+
+      let nuevaPropiedad = data[this.posPropiedad];
+      if (!nuevaPropiedad || (nuevaPropiedad && !nuevaPropiedad.id)) {
+        this.posPropiedad = 0;
+        nuevaPropiedad = data[this.posPropiedad];
+      }
+      // seteo e emision de propiedad
+      this.setPropiedad(nuevaPropiedad, this.posPropiedad);
+    })
+
   }
 }
