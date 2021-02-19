632039a696ede5acda3408472b8d6e53102d6134
Fabian Chinchilla <fb.argenis@gmail.com>
2021-02-18 22:57:11 -0600

    -- Ajustes finales mapa

--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, ViewChild } from '@angular/core';
 import { Propiedad, Location } from 'src/app/interfaces/interfaces';
 import { PropiedadesService } from 'src/app/services/propiedades.service';
+
+
 
 declare var mapboxgl: any;
 
@@ -9,43 +11,51 @@ declare var mapboxgl: any;
   templateUrl: './mapa.component.html',
   styleUrls: ['./mapa.component.css'],
 })
-export class MapaComponent implements OnInit {
+export class MapaComponent implements AfterViewInit, AfterViewChecked {
 
   coords: string;
   map: any;
   location: Location;
   @ViewChild('mapa', { static: true }) mapa: any;
 
-  constructor(private propiedadesService: PropiedadesService) { }
+  constructor(private propiedadesService: PropiedadesService) {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYXJnM25pNSIsImEiOiJja2ZtNHJ6Z2oxZmZzMnJsZHZmYTFteTAxIn0.1rKJFNCPQSYtWT0epv0euQ';
+  }
 
   ngOnInit() {
+  }
 
-    //escucha selectPropiedad
+  ngAfterViewInit() {
     this.propiedadesService.selectPropiedad.subscribe((propiedad: Propiedad) => {
-      // seteo location
       this.location = propiedad.location;
-
-      //creacion de mapa
-      if (this.location) {
-        mapboxgl.accessToken = 'pk.eyJ1IjoiYXJnM25pNSIsImEiOiJja2ZtNHJ6Z2oxZmZzMnJsZHZmYTFteTAxIn0.1rKJFNCPQSYtWT0epv0euQ';
-        this.map = new mapboxgl.Map({
-          container: this.mapa.nativeElement,
-          style: 'mapbox://styles/mapbox/streets-v11',
-          center: [this.location.longitude, this.location.latitude],
-          zoom: 15,
-          interactive: false
-        });
-        // control para pantalla completa
-        this.map.addControl(new mapboxgl.FullscreenControl());
-        // marcador en el mapa
-        const marker = new mapboxgl.Marker().setLngLat([this.location.longitude, this.location.latitude]).addTo(this.map);
-        setTimeout(() => {
-          this.map.resize();
-        }, 1500);
-      }
+      this.buildMap();
     })
   }
-  resize(){    
+  
+  ngAfterViewChecked() {
+    this.map.resize();
+  }
+
+  buildMap() {
+    let conf = {
+      container: this.mapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [this.location.longitude, this.location.latitude],
+      zoom: 15,
+      interactive: true
+    }
+
+    this.map = new mapboxgl.Map(conf)
+    this.map.on('load', () => {
+      this.map.addControl(new mapboxgl.NavigationControl())
+      // control para pantalla completa
+      this.map.addControl(new mapboxgl.FullscreenControl());
+      // marcador en el mapa
+      new mapboxgl.Marker().setLngLat([this.location.longitude, this.location.latitude]).addTo(this.map);
+    });
+  }
+  
+  resize() {
     this.map.resize();
   }
 }
